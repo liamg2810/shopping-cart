02b16aa4732156407d6345858dd9ec11a1407302
Enqu1 <114190936+Enqu1@users.noreply.github.com>
2024-09-24 18:28:30 +0100

    Basic markup for fetching list of shopping carts

---
+++ index.php
@@ -0,0 +1,68 @@
+<?php
+    $DATABASE = 'shoppingcart';
+    $USERNAME = 'root';
+    $PASSWORD = '';
+
+    $connection = new PDO('mysql:host=localhost;dbname=' . $DATABASE, $USERNAME, $PASSWORD);
+
+    function QueryCarts() {
+
+        global $connection;
+
+        $query = $connection->prepare('SELECT * FROM tblcart');
+
+        $query->execute();
+
+        $carts = $query->fetchAll();
+
+        return $carts;
+    }
+?>
+
+<html>
+<head>
+    <script src="https://cdn.tailwindcss.com"></script>
+    <title>Home</title>
+</head>
+
+<body>
+    <h1 class="text-center text-5xl font-bold">Welcome To Your Shopping Carts</h1>
+
+    
+    <div class="flex justify-center mt-10 flex-col mx-32 gap-5">
+        <a class="text-center bg-cyan-500 hover:bg-cyan-600 hover:pointer w-32 rounded-full text-lg font-bold p-2" href="addcart.php">Add Cart</a>
+        <table>
+            <thead>
+                <tr>
+                    <th class="border px-4 py-2">Cart</th>
+                    <th class="border px-4 py-2">Action</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php
+                    $carts = QueryCarts();
+
+                    if (count($carts) == 0) {
+                        echo '<tr>';
+                        echo '<td class="border px-4 py-2" colspan="2">No Carts Found</td>';
+                        echo '</tr>';
+                    }
+
+                    foreach ($carts as $cart) {
+                        echo '<tr>';
+                        echo '<td class="border px-4 py-2">' . $cart['name'] . '</td>';
+                        echo
+                        '<td class="border px-4 py-2">
+                            <div class="flex justify-left gap-2">
+                                <a class="bg-cyan-500 px-4 text-lg rounded-full" href="cart.php?id=' . $cart['id'] . '">View</a>
+                                <a class="bg-red-500  px-4  text-lg rounded-full" href="deletecart.php?id=' . $cart['id'] . '">Delete</a>
+                            </div>
+                        </td>';
+                        echo '</tr>';
+                    }
+                ?>
+            </tbody>
+        </table>
+    </div>
+</body>
+</html>
